48e3fc3deeb0327b7b486850690c316d8ccbaa9d
emmanescence <64655124+emmanescence@users.noreply.github.com>
2024-09-03 13:02:10 -0300

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
+import streamlit as st
 from datetime import datetime, timedelta
 from matplotlib.patches import FancyBboxPatch
 
